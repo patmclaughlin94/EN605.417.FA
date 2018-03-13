31cb1659464b68b99c4dfe0cea4dab640afd5d56
Pat <pat.mclaughlin94@gmail.com>
2018-03-13 01:10:28 +0000

    added register memory computation

---
+++ module6/feedForward.cu
@@ -0,0 +1,408 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+#define NUM_WEIGHTS 512
+// References:
+
+// http://www.cs.usfca.edu/~peter/cs625/code/cuda-dot/dot3.cu
+// https://devblogs.nvidia.com/using-shared-memory-cuda-cc/
+// https://github.com/JHU-EP-Intro2GPU/EN605.417.FA/blob/master/module4/host_memory.cu
+
+// Allocate constant memory for device weights
+__constant__ float const_weights_dev[NUM_WEIGHTS];
+static float const_weights_host[NUM_WEIGHTS];
+
+// Put global variables here
+int REGS_PER_BLOCK, WARP_SIZE, *MAX_THREADS_DIM;
+size_t TOTAL_GLOBAL_MEM, TOTAL_CONST_MEM;
+int MAX_THREADS_PER_BLOCK = 512;
+// Put kernel Code here
+__global__
+void dotProduct(float *weights, float *input, float *activityOutput) {
+	int id = blockDim.x * blockIdx.x + threadIdx.x;
+	int localId = threadIdx.x;
+	
+	// Initialize shared memory to store element-wise vector product
+	extern __shared__ float prod[];
+	prod[localId] = input[id] * weights[localId];
+	
+	// Synchronize threads before adding results
+	__syncthreads();
+	
+	// Loop over array to combine input
+	for(int i = blockDim.x/2; i > 0; i = i/2) {
+		if(localId < i) {
+			prod[localId] += prod[localId + i];
+		}
+		__syncthreads();
+	}
+	if(threadIdx.x == 0) {
+		activityOutput[blockIdx.x] = (prod[0] > 0) ? (prod[0]) : 0;
+	}
+}
+
+
+
+/*
+ * dotProduct2 computes dot product using constant weights vector
+ */
+__global__
+void dotProduct2(float * input, float * activityOutput) {
+	int id = blockDim.x * blockIdx.x + threadIdx.x;
+	int localId = threadIdx.x;
+	
+	extern __shared__ float prod[];
+	prod[localId] = input[id] * const_weights_dev[localId];
+	
+	__syncthreads();
+	
+	// loop over array to combine input
+	for(int i = blockDim.x/2; i > 0; i = i/2) {
+		if(localId < i) {
+			prod[localId] += prod[localId + i];
+		}
+		__syncthreads();
+	}
+	if(threadIdx.x == 0) {
+		activityOutput[blockIdx.x] = prod[0];
+	}
+}
+
+
+// Put utility functions here
+/*
+ *checkCuda: will check to see if there is an error returned by CUDA runtime
+ */
+inline
+void checkCuda(cudaError_t errMsg, const char* errContext)
+{
+	if(errMsg != cudaSuccess) {
+		fprintf(stderr, "CUDA Runtime Error From %s: %s\n", errContext, cudaGetErrorString(errMsg));
+		exit(EXIT_FAILURE);
+	}
+}
+
+/*
+ * getCUDAInfo() - originally named "getHardwareContraints in module 3
+ * 							 - this function will get CUDA information pertaining to the hardware 
+ * 							   on which we are operating... the code can then reason on these reports to determine
+ * 								 the best way to structure memory transfers between the host and device
+ */
+void getCUDAInfo() {
+	//=============================Gets number of cuda devices===========================================
+    int deviceCount = 0;
+    checkCuda(cudaGetDeviceCount(&deviceCount), "Failed deviceCount load");
+
+    // This function call returns 0 if there are no CUDA capable devices.
+    if (deviceCount == 0)
+    {
+        printf("There are no available device(s) that support CUDA\n");
+    }
+    else
+    {
+        printf("Detected %d CUDA Capable device(s)\n", deviceCount);
+    }
+//=============================Gets number of cuda devices===========================================
+    
+    // for each device found, store this device in some type of object
+    int device;
+
+    for (device = 0; device < deviceCount; device++) {
+	
+			// Sets the context of the device so that we know which device we are working with if there
+			// are multiple	
+			cudaSetDevice(device);
+  		cudaDeviceProp deviceProp;
+
+			// gets the "properties" struct that stores the properties of a device
+			// from this property struct, we can query the limitations of this device
+  		cudaGetDeviceProperties(&deviceProp, device);
+
+			printf("\nDevice: %d \"%s\"\n===========================================\n", device, deviceProp.name);
+	
+			TOTAL_GLOBAL_MEM = deviceProp.totalGlobalMem;
+			REGS_PER_BLOCK = deviceProp.regsPerBlock;
+			WARP_SIZE = deviceProp.warpSize;
+			//MAX_THREADS_PER_BLOCK = deviceProp.maxThreadsPerBlock;
+			MAX_THREADS_DIM = deviceProp.maxThreadsDim;
+			TOTAL_CONST_MEM = deviceProp.totalConstMem; 
+	printf("The %s has:\n\t-%zu total bytes of global memory\n\t-%zu bytes of constant memory\n\t-%d registers per block\n\t-%d threads per warp\n\t-A maximum of %d threads per block\n\t-A maximum thread dimension of %d x %d x %d\n", deviceProp.name, TOTAL_GLOBAL_MEM,TOTAL_CONST_MEM, REGS_PER_BLOCK, WARP_SIZE, MAX_THREADS_PER_BLOCK, MAX_THREADS_DIM[0], MAX_THREADS_DIM[1], MAX_THREADS_DIM[2]); 
+	// What I think we care about:
+	// 1. totalGlobalMem
+	// 2. regsPerBlock
+	// 3. warpSize (i.e. numThreadsPerBlock (is this equal to regsPerBlock??)
+	// 4. maxThreadsperBlock
+	// 5. maxThreadsDim[3]
+    }
+}
+
+/*
+ * allocMem
+ */
+void allocHostMem(float** inputs, float** weights1_1, float** weights1_2, float** weights1_3,
+							float** weights2_1, float** weights2_2, float** weights2_3, float** outputs, 
+							float** outputsConst, void** inputsPinned, void** weightsPinned, void** outputsPinned, int dataSize, int gridSize) {
+							
+	const unsigned int dataBytes = dataSize * sizeof(float);
+ 	const unsigned int gridBytes = gridSize * sizeof(float);
+ 	
+ 	// Host Pageable Mem
+ 	*inputs = (float*)malloc(dataBytes);
+ 	*weights1_1 = (float*)malloc(dataBytes);
+ 	*weights1_2 = (float*)malloc(dataBytes);
+ 	*weights1_3 = (float*)malloc(dataBytes);
+ 	*weights2_1 = (float*)malloc(dataBytes);
+ 	*weights2_2 = (float*)malloc(dataBytes);
+ 	*weights2_3 = (float*)malloc(dataBytes);
+ 	*outputs = (float*)malloc(gridBytes);
+ 	*outputsConst = (float*)malloc(gridBytes);
+ 
+  // Host Pinned Mem
+	checkCuda(cudaMallocHost(inputsPinned,dataBytes), "inputsPinned host allocation");
+	checkCuda(cudaMallocHost(weightsPinned,dataBytes), "weightsPinned host allocation");
+	checkCuda(cudaMallocHost(outputsPinned,gridBytes), "outputsPined host allocation");
+	
+	/*// Device Global Mem
+	checkCuda(cudaMalloc(d_inputs, dataBytes), "d_inputs device allocation");
+  checkCuda(cudaMalloc(d_weights, dataBytes), "d_weights device allocation"); 
+  checkCuda(cudaMalloc(d_outputs, gridBytes), "d_outputs device allocation");*/
+  
+}
+
+/*
+ * allocDevMem
+ */
+void allocDevMem(float** d_inputs, float** d_weights1_1, float** d_weights1_2,float** d_weights1_3,
+								 float** d_weights2_1,float** d_weights2_2,float** d_weights2_3,float** d_outputs,
+								 int dataSize, int gridSize) {
+	const unsigned int dataBytes = dataSize * sizeof(float);
+	const unsigned int gridBytes = gridSize * sizeof(float);
+	
+	// Device Inputs allocation
+	checkCuda(cudaMalloc(d_inputs, dataBytes), "d_inputs device allocation");
+	
+	// Device Weights Allocation
+	// Layer 1
+	checkCuda(cudaMalloc(d_weights1_1, dataBytes), "d_weights1_1 device allocation");
+	checkCuda(cudaMalloc(d_weights1_2, dataBytes), "d_weights1_2 device allocation");
+	checkCuda(cudaMalloc(d_weights1_3, dataBytes), "d_weights1_3 device allocation");
+	// Layer 2
+	checkCuda(cudaMalloc(d_weights2_1, dataBytes), "d_weights2_1 device allocation");
+	checkCuda(cudaMalloc(d_weights2_2, dataBytes), "d_weights2_2 device allocation");
+	checkCuda(cudaMalloc(d_weights2_3, dataBytes), "d_weights2_3 device allocation");
+	
+	// Device Output Allocation
+	checkCuda(cudaMalloc(d_outputs, gridBytes), "d_outputs device allocation");
+								 
+} 
+ 
+/*
+ *
+ */
+void populate(float** inputs, float** weights1_1, float** weights1_2, float** weights1_3, 
+							float** weights2_1, float** weights2_2, float** weights2_3, float* constWeights,
+							float** inputsPinned, float** weightsPinned,
+							float** d_inputs, float** d_weights,
+							int dataSize) {
+	time_t t;
+	srand((unsigned) time(&t));	
+	// instantiate variables with values (will be random for now)
+	for(int i = 0; i < dataSize; i++) {
+		(*inputs)[i] = rand() % 255;
+		(*inputsPinned)[i] = (*inputs)[i];
+		if(i < NUM_WEIGHTS) {
+			(*weights1_1)[i] = rand() % 30;
+			(*weights1_2)[i] = rand() % 30;
+			(*weights1_3)[i] = rand() % 30;
+			(*weights2_1)[i] = rand() % 30;
+			(*weights2_2)[i] = rand() % 30;
+			(*weights2_3)[i] = rand() % 30;
+			(*weightsPinned)[i] = (*weights1_1)[i];
+			constWeights[i] = (*weights1_1)[i];
+		}
+	}				
+}
+
+
+/*
+ * runDot
+ */
+void runDot(float* inputs, float* weights, float** outputs,
+						float* d_inputs, float* d_weights, float* d_outputs,
+						int dataSize, int gridSize, int nExecutions, int constSelector) {
+						
+	const unsigned int dataBytes = dataSize * sizeof(float);
+	const unsigned int sharedMem = MAX_THREADS_PER_BLOCK * sizeof(float);
+	//const unsigned int sharedMem = 512 * sizeof(float);
+	const unsigned int gridBytes = gridSize * sizeof(float);
+	//printf("Grid size: %d\n", gridSize);
+	//bool hasRunOnce = false;
+	float finalOut = 0;
+	for(int i = 0; i < nExecutions; i++) {
+		checkCuda(cudaMemcpy(d_inputs, inputs, dataBytes, cudaMemcpyHostToDevice), "copy inputs to device");
+		checkCuda(cudaMemcpy(d_weights, weights, dataBytes, cudaMemcpyHostToDevice), "copy weights to device");
+		//dotProduct<<<gridSize, MAX_THREADS_PER_BLOCK, MAX_THREADS_PER_BLOCK * sizeof(float)>>>(d_inputs, d_weights, d_outputs);
+		switch(constSelector) {
+			case 0: dotProduct<<<gridSize, MAX_THREADS_PER_BLOCK, MAX_THREADS_PER_BLOCK * sizeof(float)>>>(d_inputs, d_weights, d_outputs);
+			case 1: dotProduct2<<<gridSize, MAX_THREADS_PER_BLOCK, MAX_THREADS_PER_BLOCK * sizeof(float)>>>(d_inputs, d_outputs);
+			//case 1: dotProduct2<<<gridSize, 512, 512 * sizeof(float)>>>(d_inputs, d_outputs);
+		}
+		cudaThreadSynchronize();
+		// Copy result of dotProduct back to host
+		checkCuda(cudaMemcpy(*outputs, d_outputs, gridSize * sizeof(float), cudaMemcpyDeviceToHost), "copy outputs from device to host");
+	}
+	
+}
+
+
+// Put main function here
+int main(int argc, char* argv[]) {
+
+	// Allocate layer 1, 2, and 3 weights
+	// For now use 3 nodes per hidden layer
+	// 
+	// MODIFICATION: We will allocate layer weights to be constant
+	// MODIFICATION (Module 6): 2 Layers added
+	// For this module, just allocate weights for first layer
+	// Then we will add more layers and also implement convolution
+	// setup data transfer variables
+	float *inputs, *weights1_1, *weights1_2, *weights1_3,
+				*weights2_1, *weights2_2, *weights2_3,
+				*outputs, *outputsConst, *inputsPinned, 
+				*weightsPinned, *outputsPinned, *d_inputs, 
+				*d_weights1_1, *d_weights1_2, *d_weights1_3,
+				*d_weights2_1, *d_weights2_2, *d_weights2_3, *d_outputs;
+	
+	int numRuns = 1;
+	int blockSizeMult = 1;
+	if(argc == 2) {
+		//numRuns = atoi(argv[1]);
+		blockSizeMult = atoi(argv[1]);
+	}
+	printf("args %d\n", argc);
+	// Set up timing event variables
+	cudaEvent_t startEvent, stopEvent;
+
+	// Set up variables for storing duration
+	float durGlobal, durConst;
+	
+	
+	// Retrieve CUDA Device information
+	getCUDAInfo();
+	const unsigned int inputSize = (MAX_THREADS_PER_BLOCK * blockSizeMult);
+	const unsigned int blockSize = MAX_THREADS_PER_BLOCK; // it should be noted that this is an integer
+																												// multiple of warp size
+	//const unsigned int inputSize = (512 * blockSizeMult);
+	//const unsigned int blockSize = 512; // it should be noted that this is an integer
+																												// multiple of warp size
+	const unsigned int gridSize = ((inputSize/blockSize) > 0) ? (inputSize / blockSize) : 1;
+	
+	// Allocate pageable memory on host for dotproduct
+	// These get initialized by passing &inputs, &weights, &outputs, dataSize, gridSize to initHostPage
+	allocHostMem(&inputs, &weights1_1, &weights1_2, &weights1_3,
+					 &weights2_1, &weights2_2, &weights2_3, &outputs, &outputsConst,
+					 (void**)&inputsPinned, (void**)&weightsPinned, (void**)&outputsPinned,
+					 inputSize, gridSize);
+					 
+	allocDevMem(&d_inputs, &d_weights1_1, &d_weights1_2,&d_weights1_3,
+								 &d_weights2_1,&d_weights2_2,&d_weights2_3,&d_outputs,
+								 inputSize, gridSize);
+	
+	// Create events for timing
+	checkCuda(cudaEventCreate(&startEvent), "startEvent event creation");
+	checkCuda(cudaEventCreate(&stopEvent), "stopEvent event creation");
+	
+	// seed random number generator
+	time_t t;
+	srand((unsigned) time(&t));	
+	populate(&inputs, &weights1_1, &weights1_2, &weights1_3, &weights2_1, 
+					 &weights2_2, &weights2_3, const_weights_host,
+					 &inputsPinned, &weightsPinned,
+					 &d_inputs, &d_weights1_1,
+					 inputSize);
+	
+	// Copy constant weights on host to constant weights on device:
+	checkCuda(cudaMemcpyToSymbol(const_weights_dev, const_weights_host, NUM_WEIGHTS * sizeof(float)), "copy to constant dev weights");
+	
+	// Initialize timer for pageable memory 
+	checkCuda(cudaEventRecord(startEvent, 0), "startEvent event record");
+	
+	// Layer 1 feed forward
+	runDot(inputs, weights1_1, &outputs,
+				 d_inputs, d_weights1_1, d_outputs,
+				 inputSize, gridSize, numRuns, 0);
+	// Stop timer
+	checkCuda(cudaEventRecord(stopEvent, 0), "record stopEvent");
+	checkCuda(cudaEventSynchronize(stopEvent), "synchronize stopEvent");
+	
+	// Calculate duration
+	checkCuda(cudaEventElapsedTime(&durGlobal, startEvent, stopEvent), "calculate pageable duration");
+	
+	
+				 
+	// Layer 2 feed forward
+	
+	// Layer 3 feed forward
+	printf("\n\nThe dotProduct kernel executed using global memory weights %f milliseconds\n\n", durGlobal);
+	
+	// Initialize timer for pinned memory 
+	checkCuda(cudaEventRecord(startEvent, 0), "startEvent event record");			
+	 
+	runDot(inputsPinned, weights1_2, &outputsConst,
+				 d_inputs, d_weights1_2, d_outputs,
+				 inputSize, gridSize, numRuns, 1);
+	
+	// Stop timer
+	checkCuda(cudaEventRecord(stopEvent, 0), "record stopEvent");
+	checkCuda(cudaEventSynchronize(stopEvent), "synchronize stopEvent");
+	
+	// Calculate duration
+	checkCuda(cudaEventElapsedTime(&durConst, startEvent, stopEvent), "calculate pageable duration");
+	
+	printf("\n\nThe dotProduct kernel executed using constant memory weights took %f milliseconds\n\n", durConst);
+	
+	// Compute serial result
+	float serialOutput = 0.00;
+	int weightsInd = 0;
+	for(int i = 0; i < inputSize; i++) {
+		serialOutput += inputs[i]*weights1_1[weightsInd];
+		weightsInd++;
+		if (i > 0 && ((i+1) % blockSize) == 0) {
+			printf("Output %d -- Serial: %f, Parallel Paged: %f, Parallel Pinned: %f\n", ((i+1)/blockSize), serialOutput, outputs[((i+1)/blockSize) - 1], outputsConst[((i+1)/blockSize) - 1]);
+			serialOutput = 0.00;
+			weightsInd = 0;
+		}
+	}
+	
+	// Destroy timing events
+	checkCuda(cudaEventDestroy(startEvent), "destroy startEvent");
+	checkCuda(cudaEventDestroy(stopEvent), "destroy stopEvent");
+	
+	// Free device memory
+	checkCuda(cudaFree(d_inputs), "freeing device memory");
+	checkCuda(cudaFree(d_weights1_1), "freeing device memory");
+	checkCuda(cudaFree(d_weights1_2), "freeing device memory");
+	checkCuda(cudaFree(d_weights1_3), "freeing device memory");
+	checkCuda(cudaFree(d_weights2_1), "freeing device memory");
+	checkCuda(cudaFree(d_weights2_2), "freeing device memory");
+	checkCuda(cudaFree(d_weights2_3), "freeing device memory");
+	checkCuda(cudaFree(d_outputs), "freeing device memory");
+	
+	// Free host pageable memory
+	free(inputs);
+	free(weights1_1);
+	free(weights1_2);
+	free(weights1_3);
+	free(weights2_1);
+	free(weights2_2);
+	free(weights2_3);
+	free(outputs);
+	
+	// Free host pinned memory
+	checkCuda(cudaFreeHost(inputsPinned), "freeing host pinned memory");
+	checkCuda(cudaFreeHost(weightsPinned), "freeing host pinned memory");
+	checkCuda(cudaFreeHost(outputsPinned), "freeing host pinned memory");
+	
+	return(EXIT_SUCCESS);
+}
